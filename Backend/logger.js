9ad275f3a48b97e92182a6285f5f90a020cf43c1
Karavadi Pavan Kumar <ksvnspavankumar.24@gmail.com>
2025-08-01 12:45:24 +0530

    Update logger.js

--- Backend/logger.js
+++ Backend/logger.js
@@ -1,57 +1,36 @@
+// Get Token
 async function getAuthToken() {
   const credentials = {
     email: "22bq1a5472@vvit.net",
     name: "Karavadi S V N S Pavan Kumar",
-    rollNo: "22bq1a5472@vvit.net",
-    accessCode: "PnVBF",
+    rollNo: "22BQ1A5472",
+    accessCode: "PnVBFV",
     clientID: "d050e246-a5de-4fdd-8fe9-aac05d8be92a",
     clientSecret: "WgBuTdnTWUvvQYdB",
   };
-  const authUrl = 'http://20.244.56.144/evaluation-service/auth';
-
   try {
-    const response = await fetch(authUrl, {
+    const res = await fetch('http://20.244.56.144/evaluation-service/auth', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(credentials),
     });
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error('Auth Error: ${errorText}');
-    }
-    const data = await response.json();
+    const data = await res.json();
+    if (!res.ok) throw new Error('Auth Error');
     return data.access_token;
-  } catch (error) {
-    console.error(error.message);
-    return null;
-  }
+  } catch (error) { return null; }
 }
+
+// Logs
 
 async function Log(stack, level, package, message) {
   const token = await getAuthToken();
-  if (!token) {
-    console.error("Halting log: No auth token.");
-    return;
-  }
-
-  const logApiUrl = 'http://20.244.56.144/evaluation-service/logs';
-  const logData = { stack, level, package, message };
-
+  if (!token) return;
   try {
-    const response = await fetch(logApiUrl, {
+    await fetch('http://20.244.56.144/evaluation-service/logs', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization':'Bearer ${token}',
-      },
-      body: JSON.stringify(logData),
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` },
+      body: JSON.stringify({ stack, level, package, message }),
     });
-    if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error('Log API Error: ${errorText}');
-    }
-    await response.json();
-  } catch (error) {
-    console.error(error.message);
-  }
+  } catch (error) { /* Ignore */ }
 }
+module.exports = { Log };
